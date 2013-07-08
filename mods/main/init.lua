b68cb34ed6eab93c00f602de2c1213b7016204d5
Zeg9 <dazeg9@gmail.com>
2013-07-08 13:11:12 +0200

    Add overrides and stuff for item entities

--- mods/main/init.lua
+++ mods/main/init.lua
@@ -98,3 +98,34 @@ mind_mi.leaves_sounds = {
 	dig = {name="default_dig_crumbly", gain=0.4},
 	place = {name="default_place_node", gain=1.0},
 }
+
+----------------------------------
+-- Item additions and overrides --
+----------------------------------
+
+-- set_item override: always use a rotating wielditem
+minetest.registered_entities["__builtin:item"].set_item = function(self, itemstring)
+	self.itemstring = itemstring
+	local stack = ItemStack(itemstring)
+	local itemtable = stack:to_table()
+	local itemname = nil
+	if itemtable then
+		itemname = stack:to_table().name
+	end
+	self.object:set_properties({
+		is_visible = true,
+		visual = "wielditem",
+		textures = {itemname},
+		visual_size = {x=0.20, y=0.20},
+		automatic_rotate = math.pi * 0.25,
+	})
+end
+
+-- on_step additions: see what to do below
+local old_on_step = minetest.registered_entities["__builtin:item"].on_step
+minetest.registered_entities["__builtin:item"].on_step = function(self, dtime)
+	old_on_step(self,dtime)
+	-- TODO: remove item when inside lava, push when inside water
+	--       and maybe, let the user grab it by walking over it
+end
+
